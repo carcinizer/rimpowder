9cdcc0e1abe4fc958bfc3c0205eb5d4778ad150d
Michał Moniewski <mmoniewski@protonmail.com>
2025-01-06 15:47:24 +0100

    WIP collisions and porting simulation to one class, repair cmake compilation

--- include/simulation.cuh
+++ include/simulation.cuh
@@ -2,8 +2,10 @@
 
 #include <string>
 #include <cstdint>
+#include <cuda_runtime.h>
 
 constexpr size_t CHUNK_SIZE = 16;
+constexpr float GRAVITY = 9.81; // in 
 
 
 enum class ParticleType : uint8_t {
@@ -15,9 +17,11 @@ enum class ParticleType : uint8_t {
 
 struct Particle {
     ParticleType type;
+    float2 pos;
+    float2 velocity;
 
     Particle() = default;
-    Particle(uint32_t rgba);
+    Particle(uint32_t rgba, int2 pos_);
     uint32_t to_rgba() const;
 };
 
@@ -25,11 +29,21 @@ struct Chunk {
     Particle contents[CHUNK_SIZE][CHUNK_SIZE];
 };
 
+struct Collision {
+    int2 last_free; /// Last free spot found by the line drawing algorithm
+    int2 collider; /// Same as last_free if no collision occured.
+
+    __host__ __device__ Collision(int2 last_free_, int2 collider_) : last_free(last_free_), collider(collider_) {}
+    __host__ __device__ bool collided() {
+        return last_free.x == collider.x && last_free.y == collider.y;
+    }
+};
+
 
 class Simulation {
 private:
     Chunk* dev_chunks;
-    int2 dims;
+    int2 dims; /// Simulation area size, in chunks
 
 public:
     Simulation(std::string& types_filename);
@@ -42,5 +56,7 @@ public:
 
 Chunk& __host__ __device__ get_chunk(Chunk* chunks, int2 dims, int2 coord);
 Particle& __host__ __device__ get_particle(Chunk* chunks, int2 dims, int2 coord);
+Collision __device__ __host__ find_collision(Chunk* chunks, int2 dims, int2 from, int2 to);
 
-static void __global__ simulation_kernel(Chunk* chunks, unsigned width, unsigned height);
+static void __global__ simulation_kernel(Chunk* chunks, int2 dims, int time_ms);
+Collision __device__ __host__ find_collision(Chunk* chunks, int2 dims, int2 from, int2 to);
--- src/simulation.cu
+++ src/simulation.cu
@@ -4,7 +4,9 @@
 #include "simulation.cuh"
 
 
-Particle::Particle(uint32_t rgba) {
+Particle::Particle(uint32_t rgba, int2 pos_) 
+    : velocity{0.0f, 0.0f}, pos{pos_.x + 0.5f, pos_.y + 0.5f}
+{
     switch(rgba) {
         case 0xFFFF00FF:
             type = ParticleType::SAND;
@@ -59,7 +61,7 @@ Simulation::Simulation(std::string& types_filename) {
             uint32_t color = color_buffer[y*xs+x];
             int2 coord{x,y};
 
-            get_particle(host_chunks, dims, coord) = Particle(color);
+            get_particle(host_chunks, dims, coord) = Particle{color, coord};
         }
     }
 
@@ -71,33 +73,99 @@ Simulation::Simulation(std::string& types_filename) {
 }
 
 
+void __global__ simulation_kernel(Chunk* chunks, int2 dims, int2 offset, int2 stride, float time_ms) {
+    uint2 chunk{threadIdx.x * stride.x + offset.x, threadIdx.y * stride.y + offset.y};
+    float time = time_ms * 1000.0f;
+
+    for(int y = 0; y < CHUNK_SIZE; y++) {
+        for(int x = 0; x < CHUNK_SIZE; x++) {
+            int2 pos = int2{int(chunk.x + x), int(chunk.y + y)};
+
+            auto& particle = get_particle(chunks, dims, pos);
+
+            if(particle.type != ParticleType::SAND) {
+                break;
+            }
+            
+            // Calculating next position
+            float2 to_f{
+                particle.pos.x + time * particle.velocity.x,
+                particle.pos.y + time * particle.velocity.y
+            };
+            int2 to{int(to_f.x), int(to_f.y)};
+
+            auto collision = find_collision(chunks, dims, pos, to);
+
+            if(collision.collided()) {
+                particle.pos = float2{pos.x + 0.5f, pos.y + 0.5f}; // TODO lepsza aproksymacja? albo i niekoniecznie
+                particle.velocity = float2{0,0};// TODO lepsze zderzenia cząsteczek
+            }
+            else {
+                particle.pos = to_f;
+                auto vx = particle.velocity.x + time * GRAVITY;
+                auto vy = particle.velocity.y + time * GRAVITY;
+                // clamp so that no races occur between chunks
+                // TODO także przekopiować opór jeśli trzeba
+                particle.velocity.x = fminf(CHUNK_SIZE, fmaxf(float(-CHUNK_SIZE), vx));
+                particle.velocity.y = fminf(CHUNK_SIZE, fmaxf(float(-CHUNK_SIZE), vy));
+            }
+
+            get_particle(chunks, dims, to) = particle; // copy particle to new position
+            particle.type = ParticleType::VOID_; // old position is now empty
+        }
+    }
+}
+
+Collision __device__ __host__ find_collision(Chunk* chunks, int2 dims, int2 from, int2 to) {
+    int2 last_free = from;
+    int2 ptr = from;
+
+    int dx = abs(from.x - to.x);
+    int dy = abs(from.y - to.y);
+    int sx = copysignf(1.0f, to.x - from.x);
+    int sy = copysignf(1.0f, to.y - from.y);
+    int error = dx + dy;
+
+    while(true) {
+        int e2 = 2*error;
+        if(e2 >= dy) {
+            error += dy;
+            ptr.x += sx;
+        }
+        if(e2 <= dx) {
+            error += dx;
+            ptr.y += sy;
+        }
+
+        if((ptr.x == to.x && ptr.y == to.y) ) {
+            break;
+        }
+
+        if(get_particle(chunks, dims, ptr).type != ParticleType::VOID_) {
+            return Collision{last_free, ptr};
+        }
+
+        last_free = ptr;
+    }
+
+    return Collision{last_free, ptr};
+}
 
 /**
  * \brief Function calculating simulation steps
  *
  * This function calculates every step of simulation.
- * 
- * 
- *
- * 
  */
+void Simulation::step(uint32_t time_ms) {
+    int2 stride{2,2};
+    dim3 block_size{unsigned(dims.x/stride.x), unsigned(dims.y/stride.y), 1};
 
-void Simulation::step(uint32_t time_ms){
-    unsigned xs = dims.x * CHUNK_SIZE;
-    unsigned ys = dims.y * CHUNK_SIZE;
-    uint32_t* color_buffer = new uint32_t[xs*ys];
-
-    Chunk* host_chunks = new Chunk[dims.x * dims.y];
-    checkCudaErrors(cudaMemcpy(host_chunks, dev_chunks, sizeof(Chunk) * dims.x * dims.y, cudaMemcpyDeviceToHost));
-
-    for(int y = 0; y < ys; y++) {
-        for(int x = 0; x < xs; x++) {
-            int2 coord{x,y};
-            uint32_t color = get_particle(host_chunks, dims, coord).to_rgba();
-            color_buffer[y*xs+x] = color;
-        }
+    for(const auto offset : { int2{0,0}, int2{0,1}, int2{1,0}, int2{1,1} }) {
+        simulation_kernel<<<1, block_size>>>(dev_chunks, dims, offset, stride, time_ms);
+        cudaDeviceSynchronize(); // TODO upewnić się, czy to jest najlepsza metoda synchronizacji kolejnych kroków
     }
 }
+
 
 /**
  * \brief Function saving current state of the simulation
@@ -134,6 +202,7 @@ Simulation::~Simulation() {
     checkCudaErrors(cudaFree(dev_chunks));
 }
 
+
 Chunk& __host__ __device__ get_chunk(Chunk* chunks, int2 dims, int2 coord) {
     int chunk_x = coord.x/CHUNK_SIZE;
     int chunk_y = coord.y/CHUNK_SIZE;
@@ -147,5 +216,5 @@ Particle& __host__ __device__ get_particle(Chunk* chunks, int2 dims, int2 coord)
     int x_within_chunk = coord.x%CHUNK_SIZE;
     int y_within_chunk = coord.y%CHUNK_SIZE;
 
-    return chunks[chunk_y*dims.x + chunk_x].contents[chunk_y][chunk_x];
+    return chunks[chunk_y*dims.x + chunk_x].contents[y_within_chunk][x_within_chunk];
 }
