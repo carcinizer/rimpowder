2a45a0a32082a067cf9b4e24e4623098f262f79a
Miszq11 <michas.smolin@gmail.com>
2025-01-17 20:43:27 +0100

    remove sleep call to measure performance

--- src/main.cu
+++ src/main.cu
@@ -71,7 +71,7 @@ int main(int argc, char** argv) {
     // SIMULATION RELATED STUFF END
     main_window.draw();
     std::cout << "dt: " << dt << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(30));
+    // std::this_thread::sleep_for(std::chrono::milliseconds(30));
   }
   std::cout << "donna" << std::endl;
 
